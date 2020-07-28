c99994745edddcae3174d29cbea4692656546baa
wyandrew <andrewsit.wy@gmail.com>
2020-07-28 22:06:51 +0800

    changed header behavior - opacity become 1 gradually while scrolling down

--- src/App.js
+++ src/App.js
@@ -3,61 +3,68 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import Header from './components/header/header.jsx';
 
-const FontSwitch = (props) => {
-  return (
-    <div className='font-switch-container'>
-      <button className='font-switch-child' onClick={props.reduceFontSize}>  -  </button>
-      <div className='font-switch-child'>{props.fontSize}</div>
-      <button className='font-switch-child' onClick={props.incrementFontSize}>  +  </button>
-      <button className='font-switch-child' onClick={props.changeFont}>Change Font</button>
-      <div className='font-switch-child'>{props.chosenFont}</div>
-    </div>
-  );
-}
+
+
+// Font Switch for user testing only
+// const FontSwitch = (props) => {
+//   return (
+//     <div className='font-switch-container'>
+//       <button className='font-switch-child' onClick={props.reduceFontSize}>  -  </button>
+//       <div className='font-switch-child'>{props.fontSize}</div>
+//       <button className='font-switch-child' onClick={props.incrementFontSize}>  +  </button>
+//       <button className='font-switch-child' onClick={props.changeFont}>Change Font</button>
+//       <div className='font-switch-child'>{props.chosenFont}</div>
+//     </div>
+//   );
+// }
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      chosenFont: "Mulish",
-      fontSize: 19
+      // These states are used for Font Switch only
+      // chosenFont: "Mulish",
+      // fontSize: 19
     };
   }
 
-  // This part is for user testing purpose on the font style and sizes:
-  count = 0;
-  fontOptions = ['Averia Gruesa Libre', 'Mulish', 'Noto Sans JP', 'VT323'];
+  // Font Switch for user testing
+  // count = 0;
+  // fontOptions = ['Averia Gruesa Libre', 'Mulish', 'Noto Sans JP', 'VT323'];
 
-  changeFont = () => {
-    this.count += 1;
-    this.setState({ chosenFont: this.fontOptions[this.count % 4] });
-  }
+  // changeFont = () => {
+  //   this.count += 1;
+  //   this.setState({ chosenFont: this.fontOptions[this.count % 4] });
+  // }
 
-  incrementFontSize = () => {
-    if (this.state.fontSize < 50)
-    this.setState({ fontSize: this.state.fontSize +1 })
-  }
+  // incrementFontSize = () => {
+  //   if (this.state.fontSize < 50)
+  //   this.setState({ fontSize: this.state.fontSize +1 })
+  // }
 
-  reduceFontSize = () => {
-    if (this.state.fontSize > 10)
-    this.setState({ fontSize: this.state.fontSize -1 })
-  }
+  // reduceFontSize = () => {
+  //   if (this.state.fontSize > 10)
+  //   this.setState({ fontSize: this.state.fontSize -1 })
+  // }
 
 
   render() {
     return (
       <div 
         className='App'
-        style={{fontFamily: this.state.chosenFont, fontSize: this.state.fontSize+"px"}}
+        style={{
+          fontFamily: 'Mulish', // this.state.chosenFont, 
+          fontSize: 19 // this.state.fontSize+"px"
+        }}
       >
         <Header />
-        <FontSwitch 
+        {/* <FontSwitch 
           changeFont={this.changeFont}
           incrementFontSize={this.incrementFontSize} 
           reduceFontSize={this.reduceFontSize}
           chosenFont={this.state.chosenFont} 
           fontSize={this.state.fontSize}
-        />
+        /> */}
         <HomePage />
       </div>
     );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './header.scss';
 
 import logo from '../../assets/images/logo-white.png';
 
-const Header = () => (
-  <div className='header'>
-    <div className='option'>
-      ORDER
-    </div>
-    <div className='option'>
-      ABOUT
-    </div>
-    <div className='logo-container'>
-      <img className='logo' src={logo}/>
-    </div>
-    <div className='option'>
-      FAQ
-    </div>
-    <div className='option'>
-      LOCATION
-    </div>
-  </div>
-)
+class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      backgroundOpacity: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  // The header top nav-bar will change its background opacity on scrolling when the 
+  // window page Y offset position falls in a certain range (e.g. 270-530)
+  handleScroll = () => {
+    let pos = window.pageYOffset;
+    if (pos < 270) this.setState({backgroundOpacity: 0}) 
+    else if (pos > 530) this.setState({backgroundOpacity: 1}) 
+    else this.setState({backgroundOpacity: (pos-270) / 260});
+  }
+
+  render() {
+    return(
+      <nav 
+        className='header' 
+        style={{backgroundColor: `rgba(0, 0, 0, ${this.state.backgroundOpacity})`}}
+      >
+        <div className='option'>
+          ORDER
+        </div>
+        <div className='option'>
+          ABOUT
+        </div>
+        <div className='logo-container'>
+          <img className='logo' src={logo}/>
+        </div>
+        <div className='option'>
+          FAQ
+        </div>
+        <div className='option'>
+          LOCATION
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Header;
 
